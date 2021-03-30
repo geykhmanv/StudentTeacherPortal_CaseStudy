d1707c1c7901d5e73b35c3913fdb3cad1a14bed8
Victoria Geykhman <geykhmanv@gmail.com>
2021-03-30 15:39:34 -0400

    SQL file added

--- src/main/resources/SQL/StudentTeacherPortal-Create.sql
+++ src/main/resources/SQL/StudentTeacherPortal-Create.sql
@@ -11,6 +11,19 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+
+-- Dumping database structure for student_teacher_portal
+CREATE DATABASE IF NOT EXISTS `student_teacher_portal` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `student_teacher_portal`;
+
+-- Dumping structure for table student_teacher_portal.course
+CREATE TABLE IF NOT EXISTS `course` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `course_name` varchar(255) DEFAULT NULL,
+  `max_students` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+
 -- Dumping data for table student_teacher_portal.course: ~10 rows (approximately)
 /*!40000 ALTER TABLE `course` DISABLE KEYS */;
 INSERT INTO `course` (`id`, `course_name`, `max_students`) VALUES
@@ -26,14 +39,41 @@ INSERT INTO `course` (`id`, `course_name`, `max_students`) VALUES
 	(10, 'Software Engineering', 22);
 /*!40000 ALTER TABLE `course` ENABLE KEYS */;
 
+-- Dumping structure for table student_teacher_portal.course_teacher
+CREATE TABLE IF NOT EXISTS `course_teacher` (
+  `teacher_id` bigint(20) NOT NULL,
+  `courses_id` bigint(20) NOT NULL,
+  UNIQUE KEY `UK_21fyk4jbf6hgstelbys249psr` (`courses_id`),
+  KEY `FKsb9lnio7h0a885b871ogw0ajg` (`teacher_id`),
+  CONSTRAINT `FKl9e7vyic0tqi9oapijwsm9oh1` FOREIGN KEY (`courses_id`) REFERENCES `course` (`id`),
+  CONSTRAINT `FKsb9lnio7h0a885b871ogw0ajg` FOREIGN KEY (`teacher_id`) REFERENCES `teacher` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- Dumping data for table student_teacher_portal.course_teacher: ~4 rows (approximately)
 /*!40000 ALTER TABLE `course_teacher` DISABLE KEYS */;
 INSERT INTO `course_teacher` (`teacher_id`, `courses_id`) VALUES
 	(1, 3),
-	(1, 9),
+	(1, 7),
 	(2, 2),
 	(2, 5);
 /*!40000 ALTER TABLE `course_teacher` ENABLE KEYS */;
+
+-- Dumping structure for table student_teacher_portal.student
+CREATE TABLE IF NOT EXISTS `student` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `address` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `date_of_enrollment` date DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `state` varchar(255) DEFAULT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `zipcode` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table student_teacher_portal.student: ~2 rows (approximately)
 /*!40000 ALTER TABLE `student` DISABLE KEYS */;
@@ -41,6 +81,24 @@ INSERT INTO `student` (`id`, `address`, `city`, `date_of_enrollment`, `email`, `
 	(1, '55 Spruce Street', 'London', '2021-03-30', 'brian@gmail.com', 'Brian', 'Johnson', 'brian1', '444-444-4444', 'MA', 'brian1', '12345'),
 	(2, '78 Null Road', 'Paris', '2019-05-30', 'rita@yahoo.com', 'Rita', 'Brown', 'rita1', '999-999-9999', 'OH', 'rita1', '12345');
 /*!40000 ALTER TABLE `student` ENABLE KEYS */;
+
+-- Dumping structure for table student_teacher_portal.teacher
+CREATE TABLE IF NOT EXISTS `teacher` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `address` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `date_of_hire` date DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `state` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `zipcode` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table student_teacher_portal.teacher: ~2 rows (approximately)
 /*!40000 ALTER TABLE `teacher` DISABLE KEYS */;
